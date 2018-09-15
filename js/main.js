693cac6f42958ab6899f3818753cd3caeef73784
ovciarik <martin.ovciarik@gmail.com>
2018-09-15 16:28:07 +0100

    Import/Export

--- js/main.js
+++ js/main.js
@@ -1,100 +1,96 @@
-let STATE = []
-let PLAY_PAUSE = false
-let DIMENSION_X = 120
-let DIMENSION_Y = 120
+const DIMENSION_X = 150 
+const DIMENSION_Y = 150
 
-let ZOOM_LEVEL = 10
+let STATE = []
+let PLAY_PAUSE = true
+let ZOOM_LEVEL = 5
 
 function* intGenerator(start, stop){
-  while (start < stop) {
-      yield start;
-      start += 1
-  }
+    while (start < stop) {
+        yield start
+        start += 1
+    }
 }
 
-function tableCreate(state) {
-    let universe = document.getElementById('universe');
-    let tbl = document.createElement('table');
-
-    tbl.style.border = '1px solid #2a2b2f';
-    tbl.style['border-collapse'] = 'collapse';
-    posX = 0
-
-    // for (let pos_x of intGenerator(0, 100)){
+function drawTable(state) {
+    let universe = document.getElementById('universe')
+    let table = document.createElement('table')
+    table.style.border = '1px solid #2a2b2f'
+    table.style['border-collapse'] = 'collapse'
+    let x = 0
     state.forEach(row => {
-        let posY = 0
-        let tr = document.createElement('tr');
+        let y = 0
+        let tr = document.createElement('tr')
         row.forEach(_ => {
-            let td = document.createElement('td');
+            let td = document.createElement('td')
             td.style['height'] = `${ZOOM_LEVEL}px`
             td.style['min-width'] = `${ZOOM_LEVEL}px`
             td.style.border = '1px solid #2a2b2f'
             td.style.background = '#4f4f57'
-            td.id = `${posX}_${posY}`
+            td.id = `${x}_${y}`
             tr.appendChild(td)
-            posY += 1
+            y += 1
         })
-        tbl.appendChild(tr)
-        posX += 1
-  })
-  universe.appendChild(tbl)
+        table.appendChild(tr)
+        x += 1
+    })
+    universe.appendChild(table)
 }
 
 function redrawTable(state){
-    let pos_x = 0
-    for (row of state){
-        let pos_y = 0
-        for (cell of row){
-            let elem = document.getElementById(`${pos_x}_${pos_y}`);
+    let x = 0
+    state.forEach(row => {
+        let y = 0
+        row.forEach( cell => {
+            let elem = document.getElementById(`${x}_${y}`)
             if (cell === 0) {
-                elem.style.background = '#4f4f57'
+                if (elem.style.backgroundColor === 'rgb(46, 139, 87)'){
+                    elem.style.background = '#4f4f57'
+                }
             } else {
-                elem.style.background = '#2e8b57'
+                if (elem.style.backgroundColor === 'rgb(79, 79, 87)'){
+                    elem.style.background = '#2e8b57'
+                }
             }
-            pos_y += 1
-        }
-        pos_x += 1
-    }
+            y += 1
+        })
+        x += 1
+    })
     return state
 }
 
 function rezoomTable(state){
-    let pos_x = 0
-    for (row of state){
-        let pos_y = 0
-        for (cell of row){
-            let elem = document.getElementById(`${pos_x}_${pos_y}`);
+    let x = 0
+    state.forEach(row => {
+        let y = 0
+        row.forEach(_ => {
+            let elem = document.getElementById(`${x}_${y}`)
             elem.style['height'] = `${ZOOM_LEVEL}px`
             elem.style['min-width'] = `${ZOOM_LEVEL}px`
-            pos_y += 1
-        }
-        pos_x += 1
-    }
+            y += 1
+        })
+        x += 1
+    })
     return state
-
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function getNewState(state){
+function calculateNextState(state){
     let newState = []
     let pos_x = 0
-    for (row of state){
+    state.forEach(row => {
         let newRow = []
         let pos_y = 0
-        for (cell of row){
-            n0 = (state[pos_x-1] && state[pos_x-1][pos_y-1]) || 0
-            n1 = (state[pos_x-1] && state[pos_x-1][pos_y]) || 0
-            n2 = (state[pos_x-1] && state[pos_x-1][pos_y+1]) || 0
-            n3 = (state[pos_x] && state[pos_x][pos_y-1]) || 0
-            n4 = (state[pos_x] && state[pos_x][pos_y+1]) || 0
-            n5 = (state[pos_x+1] && state[pos_x+1][pos_y-1]) || 0
-            n6 = (state[pos_x+1] && state[pos_x+1][pos_y]) || 0
-            n7 = (state[pos_x+1] && state[pos_x+1][pos_y+1]) || 0
+        row.forEach(cell => {
+            let n0 = (state[pos_x-1] && state[pos_x-1][pos_y-1]) || 0
+            let n1 = (state[pos_x-1] && state[pos_x-1][pos_y]) || 0
+            let n2 = (state[pos_x-1] && state[pos_x-1][pos_y+1]) || 0
+            let n3 = (state[pos_x] && state[pos_x][pos_y-1]) || 0
+            let n4 = (state[pos_x] && state[pos_x][pos_y+1]) || 0
+            let n5 = (state[pos_x+1] && state[pos_x+1][pos_y-1]) || 0
+            let n6 = (state[pos_x+1] && state[pos_x+1][pos_y]) || 0
+            let n7 = (state[pos_x+1] && state[pos_x+1][pos_y+1]) || 0
 
-            aliveNeighbors = [n0, n1, n2, n3, n4, n5, n6, n7].reduce((a, b) => {return a+b})
+            let aliveNeighbors = [n0, n1, n2, n3, n4, n5, n6, n7].reduce((a, b) => {return a+b})
 
             if (((cell === 0) && (aliveNeighbors !== 3)) || ((cell === 1)&&(aliveNeighbors <= 1)) || ((cell === 1)&&(aliveNeighbors >= 4))){
                 newRow.push(0)
@@ -102,60 +98,42 @@ function getNewState(state){
                 newRow.push(1)
             }
             pos_y += 1
-        }
+        })
         newState.push(newRow)
         pos_x += 1
-    }
+    })
     return newState
 }
 
-function main_loop(){
-    if (PLAY_PAUSE){
-        STATE = getNewState(STATE)
-        redrawTable(STATE)
-    }
-
-    window.setTimeout(main_loop, 16)
-}
-
-
 function playPauseToggle(){
     PLAY_PAUSE = !PLAY_PAUSE
-    // console.log(PLAY_PAUSE)
 }
 
 function nextStep(){
     if (!PLAY_PAUSE){
-        STATE = getNewState(STATE)
+        STATE = calculateNextState(STATE)
         redrawTable(STATE)
     }
 }
 
 function editCell(cell){
-    // STATE
-    // console.log(cell)
-    // console.log()
-    let posX = cell['target']['id'].split('_')[0]
-    let posY = cell['target']['id'].split('_')[1]
-    console.log(STATE[posX][posY])
-    let currentState = STATE[posX][posY]
+    let x = cell['target']['id'].split('_')[0]
+    let y = cell['target']['id'].split('_')[1]
+    let currentState = STATE[x][y]
+
     if (currentState === 1) {
-        STATE[posX][posY] = 0
+        STATE[x][y] = 0
     } else {
-        STATE[posX][posY] = 1
+        STATE[x][y] = 1
     }
-
     redrawTable(STATE)
-
-    console.log(STATE[posX][posY])
-
 }
 
 function generateInitialState(width, height){
-    table = []
-    for (let x of intGenerator(0, width)){
-        row = []
-        for (let y of intGenerator(0, height)){
+    let table = []
+    for (let _ of intGenerator(0, width)){
+        let row = []
+        for (let _ of intGenerator(0, height)){
             row.push(0)
         
         }
@@ -165,14 +143,12 @@ function generateInitialState(width, height){
 
 }
 
-
 function generatePattern(state, pattern, offsetX, offsetY){
     let x = 0
     pattern.forEach(row => {
         let y = 0
         row.forEach(cell => {
             state[x+offsetX][y+offsetY] = cell
-
             y += 1
         })
         x += 1
@@ -181,7 +157,7 @@ function generatePattern(state, pattern, offsetX, offsetY){
 }
 
 function generateChaosPattern(state){
-    let offsetX = 50
+    let offsetX = 75
     let offsetY = 30
     let pattern = [
         [1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,0,0,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,0,1,1,1,1,1]
@@ -228,51 +204,80 @@ function zoomIn(){
     ZOOM_LEVEL += 1
     rezoomTable(STATE)
 }
+
 function zoomOut(){
     ZOOM_LEVEL -= 1
     rezoomTable(STATE)
 }
 
+
+function readSuccess(content){
+    let pattern = JSON.parse(content.target.result)['state']
+    STATE = generateInitialState(DIMENSION_X, DIMENSION_Y)
+    STATE =  generatePattern(STATE, pattern, 0, 0)
+    redrawTable(STATE)
+}
+
+function fnImport(param){
+    let file = param.target.files[0]
+    let reader = new FileReader()
+    reader.readAsText(file)
+    reader.onload = readSuccess
+}
+
+function fnExport(){
+    let filename = 'gol.json'
+    let jsonFile = JSON.stringify({'state': STATE})
+    let blob = new Blob([jsonFile], { type: 'text/json;charset=utf-8;' })
+    let link = document.createElement('a')
+    let url = URL.createObjectURL(blob)
+    link.setAttribute('href', url)
+    link.setAttribute('download', filename)
+    link.style.visibility = 'hidden'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+}
+
 function init(){
+    // Add event listeners
+    document.getElementById('playPauseButton').onclick = playPauseToggle
+    document.getElementById('nextStepButton').onclick = nextStep
+    document.getElementById('clearButton').onclick = clear
+    document.getElementById('chaosPatternButton').onclick = chaosPattern
+    document.getElementById('gliderGunButton').onclick = gliderGun
+    document.getElementById('zoomInButton').onclick = zoomIn
+    document.getElementById('zoomOutButton').onclick = zoomOut
+    document.getElementById('importButton').onchange = fnImport
+    document.getElementById('exportButton').onclick = fnExport
+
+    // generate initial state
     STATE = generateInitialState(DIMENSION_X, DIMENSION_Y)
     STATE = generateChaosPattern(STATE)
-    // console.log(STATE)
-    playPauseButton = document.getElementById('playPauseButton')
-    playPauseButton.onclick = playPauseToggle
 
-    nextStepButton = document.getElementById('nextStepButton')
-    nextStepButton.onclick = nextStep
-
-    clearButton = document.getElementById('clearButton')
-    clearButton.onclick = clear
-
-    chaosPatternButton = document.getElementById('chaosPatternButton')
-    chaosPatternButton.onclick = chaosPattern
-
-    gliderGunButton = document.getElementById('gliderGunButton')
-    gliderGunButton.onclick = gliderGun
-
-    zoomInButton = document.getElementById('zoomInButton')
-    zoomInButton.onclick = zoomIn
-
-    zoomOutButton = document.getElementById('zoomOutButton')
-    zoomOutButton.onclick = zoomOut
-
-    tableCreate(STATE)
+    // generate table
+    drawTable(STATE)
     redrawTable(STATE)
 
-    let pos_x = 0
-    for (row of STATE){
-        let pos_y = 0
-        for (cell of row){
-            let elem = document.getElementById(`${pos_x}_${pos_y}`);
+    // add event listener to every cell
+    let x = 0
+    STATE.forEach( row => {
+        let y = 0
+        row.forEach( cell => {
+            let elem = document.getElementById(`${x}_${y}`)
             elem.onclick = editCell
-            pos_y += 1
-        }
-        pos_x += 1
-    }
-    // add event listenr to every cell
+            y += 1
+        })
+        x += 1
+    })
+}
 
+function main_loop(){
+    if (PLAY_PAUSE){
+        STATE = calculateNextState(STATE)
+        redrawTable(STATE)
+    }
+    window.setTimeout(main_loop, 1)
 }
 
 function main(){
